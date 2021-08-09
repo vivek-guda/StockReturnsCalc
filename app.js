499607635c34e475b76ddcffff341b041c9a06ae
vivek-guda <vivekreddy1000@gmail.com>
2021-08-09 22:16:09 +0530

    added changes to container background

--- app.js
+++ app.js
@@ -4,20 +4,28 @@ const stockQuan = document.querySelector("#stock-quan");
 const currenPrice = document.querySelector("#current-price");
 const output = document.querySelector("#output");
 const outputDiv = document.querySelector("#output-div");
+const container = document.querySelector("#container");
 
 inpForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  container.classList.remove("happy");
+  container.classList.remove("sad");
   let pp = purchasePrice.value;
   let sq = stockQuan.value;
   let cp = currenPrice.value;
   let profitPerc = ((cp - pp) / pp) * 100;
   let profitPrice = (cp - pp) * sq;
   outputDiv.style.display = "block";
+  console.log(profitPerc);
   if (profitPerc < 0) {
-    output.innerText = `You lost ${abs(profitPerc)}%. Your total loss is ₹${abs(
-      profitPrice
-    )}.`;
+    output.innerText = `You lost ${-profitPerc}%. Your total loss is ₹${-profitPrice}. 😔`;
+    if (profitPerc <= -50) {
+      container.classList.add("sad");
+    }
   } else {
-    output.innerText = `You gained ${profitPerc}%. Your total profit is ₹${profitPrice}.`;
+    output.innerText = `You gained ${profitPerc}%. Your total profit is ₹${profitPrice}. 🎉`;
+    if (profitPerc >= 50) {
+      container.classList.add("happy");
+    }
   }
 });
